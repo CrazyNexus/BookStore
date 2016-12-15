ef86549065814297fe312710f49d25ffe6a6ce98
Thomas Dubiel <td@crazynexus.de>
2016-12-15 12:48:53 +0100

    initial commit

---
+++ BookStore.iOS/BooksTableViewController.cs
@@ -0,0 +1,20 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace BookStore.iOS
+{
+   public partial class BooksTableViewController : UITableViewController
+   {
+
+      public BooksTableViewController(IntPtr handle) : base(handle)
+      {
+
+      }
+   }
+
+   public class Book
+   {
+
+   }
+}
