a855c68845bba2e6edfdf213cdfe65c8b30aad0e
Thomas Dubiel <td@crazynexus.de>
2016-12-15 15:18:17 +0100

    add an Android solution

---
+++ BookStore.Droid/BooksAdapter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Widget;
+using Java.Lang;
+
+namespace BookStore.Droid
+{
+   public class BooksAdapter : BaseAdapter
+   {
+      public List<Book> books;
+      Activity activity;
+
+      public BooksAdapter(Activity activity)
+      {
+         books = new List<Book>();
+         this.activity = activity;
+      }
+
+      public BooksAdapter(Activity activity, List<Book> books)
+      {
+         this.books = books;
+         this.activity = activity;
+      }
+
+
+      public override int Count
+      {
+         get
+         {
+            return books.Count;
+         }
+      }
+
+      public override global::Android.Views.View GetView(int position, global::Android.Views.View convertView, global::Android.Views.ViewGroup parent)
+      {
+         var view = convertView ?? activity.LayoutInflater.Inflate(Resource.Layout.BookCell, parent, false);
+
+         var nameLabel = view.FindViewById<TextView>(Resource.Id.bookNameTextView);
+         var yearLabel = view.FindViewById<TextView>(Resource.Id.boockYearTextView);
+         var authorLabel = view.FindViewById<TextView>(Resource.Id.bookAuthorTextView);
+         var publisherLabel = view.FindViewById<TextView>(Resource.Id.bookPublisherTextView);
+
+         var data = books[position];
+
+         nameLabel.Text = data.name;
+         yearLabel.Text = data.year.ToString();
+         authorLabel.Text = data.author;
+         publisherLabel.Text = data.editor;
+
+         return view;
+      }
+
+      public override Java.Lang.Object GetItem(int position)
+      {
+         return null;
+      }
+
+      public override long GetItemId(int position)
+      {
+         return books[position].id;
+      }
+
+   }
+}
---
+++ BookStore.Droid/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace BookStore.Droid
+{
+   [Activity(Label = "BookStore.Droid", MainLauncher = true, Icon = "@mipmap/icon")]
+   public class MainActivity : ListActivity
+   {
+      List<Book> bookList;
+      protected override void OnCreate(Bundle savedInstanceState)
+      {
+         base.OnCreate(savedInstanceState);
+
+         bookList = new List<Book>();
+
+         bookList.Add(new Book()
+         {
+            author = "Thomas Dubiel",
+            name = "Diplomarbeit",
+            editor = "Hans-Jürgen Pfenning",
+            year = 1996
+         });
+         bookList.Add(new Book()
+         {
+            author = "Silja Dubiel",
+            name = "Regeln der Ehe",
+            editor = "Mariana Schumann",
+            year = 2012
+         });
+         bookList.Add(new Book()
+         {
+            author = "John Doe",
+            name = "Wer bin ich?",
+            editor = "John Smith",
+            year = 2014
+         });
+
+         var listAdapter = new BooksAdapter(this, bookList);
+         ListAdapter = listAdapter;
+      }
+   }
+
+   public class Book
+   {
+      public long id;
+      public string name;
+      public string author;
+      public string editor;
+      public int year;
+
+      public override string ToString()
+      {
+         return string.Format("{0} - {1} - {2} - {3}", this.name, this.author, this.editor, this.year);
+      }
+   }
+}
+
