084dd066f75be0cef57eff289fdf3a77cbe3f67c
Thomas Dubiel <td@crazynexus.de>
2016-12-16 14:25:37 +0100

    add the Android app

---
+++ BookStore.Droid/DetailsActivity.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BookStore.Droid
+{
+   [Activity(Label = "DetailsActivity")]
+   public class DetailsActivity : Activity
+   {
+      TextView bookName;
+      ImageView bookCoverImageView;
+      Book selectedBook;
+
+      protected override void OnCreate(Bundle savedInstanceState)
+      {
+         base.OnCreate(savedInstanceState);
+         SetContentView(Resource.Layout.Details);
+
+         bookName = FindViewById<TextView>(Resource.Id.bookName2);
+         bookCoverImageView = FindViewById<ImageView>(Resource.Id.bookCoverImageView);
+
+         selectedBook = new Book();
+         selectedBook.name = Intent.Extras.GetString("selected_book");
+         selectedBook.imageId = Intent.Extras.GetInt("selected_image_id");
+
+         bookName.Text = selectedBook.name;
+         bookCoverImageView.SetImageResource(selectedBook.imageId);
+
+      }
+   }
+}
--- BookStore.Droid/MainActivity.cs
+++ BookStore.Droid/MainActivity.cs
@@ -13,6 +13,8 @@ namespace BookStore.Droid
       {
          base.OnCreate(savedInstanceState);
 
+         ListView.ItemClick += ListView_ItemClick;
+
          bookList = new List<Book>();
 
          bookList.Add(new Book()
@@ -20,25 +22,49 @@ namespace BookStore.Droid
             author = "Thomas Dubiel",
             name = "Diplomarbeit",
             editor = "Hans-Jürgen Pfenning",
-            year = 1996
+            year = 1996,
+            imageId = Resource.Drawable.Bild1
          });
          bookList.Add(new Book()
          {
             author = "Silja Dubiel",
             name = "Regeln der Ehe",
             editor = "Mariana Schumann",
-            year = 2012
+            year = 2012,
+            imageId = Resource.Drawable.Bild2
          });
          bookList.Add(new Book()
          {
             author = "John Doe",
             name = "Wer bin ich?",
             editor = "John Smith",
-            year = 2014
+            year = 2014,
+            imageId = Resource.Drawable.Bild3
          });
 
          var listAdapter = new BooksAdapter(this, bookList);
          ListAdapter = listAdapter;
+      }
+
+      void ListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+      {
+         if (e != null)
+         {
+            var selectedBook = bookList[e.Position];
+            var intent = new global::Android.Content.Intent(this, typeof(DetailsActivity));
+
+            Bundle bundle = new Bundle();
+            bundle.PutString("selected_book", selectedBook.name);
+            bundle.PutInt("selected_image_id", selectedBook.imageId);
+
+            intent.PutExtras(bundle);
+
+            StartActivity(intent);
+         }
+         else
+         {
+            return;
+         }
       }
    }
 
@@ -49,6 +75,7 @@ namespace BookStore.Droid
       public string author;
       public string editor;
       public int year;
+      public int imageId;
 
       public override string ToString()
       {
